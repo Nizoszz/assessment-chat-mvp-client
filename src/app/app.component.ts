36c4035b9d83e325bd001d5ba879ac09151b3f9b
Andrew da Silva <andrewairamdasilva@gmail.com>
2025-04-10 23:39:48 -0300

    feat(Scaffolding): Init config project

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
+import { ChatComponent } from "./chat/chat.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ChatComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'assessment-chat-mvp-client';
+  private routeSubscription?: Subscription;
+  constructor(
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.routeSubscription = this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.getRouteTitle(this.activatedRoute))
+      )
+      .subscribe((title) => (this.title = title));
+  }
+  ngOnDestroy(): void {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+  private getRouteTitle(route: ActivatedRoute): string {
+    let child = route;
+    while (child.firstChild) {
+      child = child.firstChild;
+    }
+    return child.snapshot.data['title'] || 'Default Title';
+  }
+}
---
+++ src/app/app.config.ts
@@ -0,0 +1,24 @@
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideNgxMask } from 'ngx-mask';
+import { provideToastr } from 'ngx-toastr';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideNgxMask({}),
+    provideToastr()
+  ],
+};
